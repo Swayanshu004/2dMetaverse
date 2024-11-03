508894308f09f396aa25b6d7b79bedb4dd3dfa51
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2024-11-04 00:26:44 +0530

    remove scrollbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="bg-neutral-700 w-max-fill h-[70vh] m-10 text-black rounded-xl">
+    <div className="bg-neutral-700 w-max-fill h-[70vh] mx-10 mb-7 mt-5 text-black rounded-xl">
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Link from 'next/link'
 
 function Navbar() {
-  return (
-    <div className='flex flex-col items-center justify-between px-10 py-4 w-screen sm:flex-row '>
-        <Link href="#">
-            <h1 className="text-4xl font-semibold text-cyan-400">Virtual-Box</h1>
-        </Link>
-        <div className='mt-4 w-full lg:mt-0 lg:w-1/3'>
-            <ul className='w-full flex items-center justify-end gap-3 md:gap-5 '>
-                <li>
-                    <div>
-                        <input 
-                        type="text"
-                        placeholder='search' 
-                        className='py-2 px-3 rounded-md bg-neutral-800 focus:border-0 font-mono'/>
-                    </div>
-                </li>
-                <li><Link href="#" className='text-sm md:text-md hover:text-cyan-400 border-2 hover:border-cyan-400 rounded-lg px-2 md:px-5 py-2 font-mono'>Login</Link></li>
-                <li>
-                    <Link href="#">
-                        <div className='w-10 h-10 bg-cyan-500 rounded-lg hover:border-2'></div>
-                    </Link>
-                </li>
-            </ul>
+    const [logedin, setlogedin] = useState(true);
+    return (
+        <div className='flex flex-col items-center justify-between px-10 py-4 w-screen sm:flex-row '>
+            <Link href="#">
+                <h1 className="text-4xl font-semibold text-cyan-400">Virtual-Box</h1>
+            </Link>
+            <div className='mt-4 w-full lg:mt-0 lg:w-1/3'>
+                <ul className='w-full flex items-center justify-end gap-3 md:gap-5 '>
+                    <li>
+                        <div>
+                            <input 
+                            type="text"
+                            placeholder='search' 
+                            className='py-2 px-3 rounded-md bg-neutral-800 focus:border-0 font-mono'/>
+                        </div>
+                    </li>
+                    <li><Link href="#" className='text-sm md:text-md hover:text-cyan-400 border-2 hover:border-cyan-400 rounded-lg px-2 md:px-5 py-2 font-mono'>{logedin ? "Get Started" : "Login"}</Link></li>
+                    <li>
+                        <Link href="#">
+                            <div className='w-10 h-10 bg-cyan-500 rounded-lg hover:border-2'></div>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
